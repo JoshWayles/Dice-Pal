275b35e9cfc5800283bca10a09eb6f1504a40a1e
Josh Wayles <Joshwayles@gmail.com>
2024-09-15 22:28:36 +0100

    Adding preload to images

--- script.js
+++ script.js
@@ -1,91 +1,105 @@
+//Image preload
+function preloadImages(im_url) {
+  let img = new Image();
+  img.src = im_url;
+}
+
+preloadImages("images/d4.png");
+preloadImages("images/d6.png");
+preloadImages("images/d8.png");
+preloadImages("images/d10.png");
+preloadImages("images/d12.png");
+preloadImages("images/d20.png");
+preloadImages("images/d100.png");
+preloadImages("images/no-dice.png");
+
 //Dice button
+const diceButton = document.querySelector("#dice-btn");
+diceButton.addEventListener("click", buttonTest);
 
-const diceButton = document.querySelector("#dice-btn")
-diceButton.addEventListener("click", buttonTest)
-
-const diceSize = document.querySelector("#dice-size")
-const diceImage = document.querySelector("#dice-image")
-const diceAnimation = document.querySelector(".dice-image")
+const diceSize = document.querySelector("#dice-size");
+const diceImage = document.querySelector("#dice-image");
+const diceAnimation = document.querySelector(".dice-image");
 
 const diceImageArray = ["images/d4.png", "images/d6.png", "images/d8.png",
   "images/d10.png", "images/d12.png", "images/d20.png", "images/100.png",
   "images/no-dice.png"
-]
+];
 
+//Dice button code
 function buttonTest() {
 
-  let dice = Number(diceSize.value)
+  let dice = Number(diceSize.value);
+
+  if (dice === 4){
+    diceImage.style.top = "40%";
+  }else{
+    diceImage.style.top = "50%";
+  }
 
   if (dice === 0) {
-    diceImage.src = diceImageArray[7]
-    diceImage.style.top = "50%"
-    diceAnimation.style.animation = "noDice 500ms"
-    setTimeout(removeAnimation, 500)
-    diceButton.textContent = "Pick A Size"
-    diceButton.style.fontSize = "40px"
+    diceImage.src = diceImageArray[7];
+    diceAnimation.style.animation = "noDice 500ms";
+    setTimeout(removeAnimation, 500);
+    diceButton.textContent = "Pick A Size";
+    diceButton.style.fontSize = "40px";
   } if (dice === 4) {
-    diceImage.src = diceImageArray[0]
-    diceImage.style.top = "40%"
-    diceAnimation.style.animation = "rollDice 500ms"
-    setTimeout(removeAnimation, 500)
-    diceButton.textContent = diceResult(dice)
-    diceButton.style.fontSize = "90px"
+    diceImage.src = diceImageArray[0];
+    diceAnimation.style.animation = "rollDice 500ms";
+    setTimeout(removeAnimation, 500);
+    diceButton.textContent = diceResult(dice);
+    diceButton.style.fontSize = "90px";
   } else if (dice === 6) {
-    diceImage.src = diceImageArray[1]
-    diceImage.style.top = "50%"
-    diceAnimation.style.animation = "rollDice 500ms"
-    setTimeout(removeAnimation, 500)
-    diceButton.textContent = diceResult(dice)
-    diceButton.style.fontSize = "90px"
+    diceImage.src = diceImageArray[1];
+    diceAnimation.style.animation = "rollDice 500ms";
+    setTimeout(removeAnimation, 500);
+    diceButton.textContent = diceResult(dice);
+    diceButton.style.fontSize = "90px";
   } else if (dice === 8) {
-    diceImage.src = diceImageArray[2]
-    diceImage.style.top = "50%"
-    diceAnimation.style.animation = "rollDice 500ms"
-    setTimeout(removeAnimation, 500)
-    diceButton.textContent = diceResult(dice)
-    diceButton.style.fontSize = "90px"
+    diceImage.src = diceImageArray[2];
+    diceAnimation.style.animation = "rollDice 500ms";
+    setTimeout(removeAnimation, 500);
+    diceButton.textContent = diceResult(dice);
+    diceButton.style.fontSize = "90px";
   } else if (dice === 10) {
-    diceImage.src = diceImageArray[3]
-    diceImage.style.top = "50%"
-    diceAnimation.style.animation = "rollDice 500ms"
-    setTimeout(removeAnimation, 500)
-    diceButton.textContent = diceResult(dice)
-    diceButton.style.fontSize = "90px"
+    diceImage.src = diceImageArray[3];
+    diceAnimation.style.animation = "rollDice 500ms";
+    setTimeout(removeAnimation, 500);
+    diceButton.textContent = diceResult(dice);
+    diceButton.style.fontSize = "90px";
   } else if (dice === 12) {
-    diceImage.src = diceImageArray[4]
-    diceImage.style.top = "50%"
-    diceAnimation.style.animation = "rollDice 500ms"
-    setTimeout(removeAnimation, 500)
-    diceButton.textContent = diceResult(dice)
-    diceButton.style.fontSize = "90px"
+    diceImage.src = diceImageArray[4];
+    diceAnimation.style.animation = "rollDice 500ms";
+    setTimeout(removeAnimation, 500);
+    diceButton.textContent = diceResult(dice);
+    diceButton.style.fontSize = "90px";
   } else if (dice === 20) {
-    diceImage.src = diceImageArray[5]
-    diceImage.style.top = "50%"
-    diceAnimation.style.animation = "rollDice 500ms"
-    setTimeout(removeAnimation, 500)
-    diceButton.textContent = diceResult(dice)
-    diceButton.style.fontSize = "90px"
+    diceImage.src = diceImageArray[5];
+    diceAnimation.style.animation = "rollDice 500ms";
+    setTimeout(removeAnimation, 500);
+    diceButton.textContent = diceResult(dice);
+    diceButton.style.fontSize = "90px";
   } else if (dice === 100) {
-    diceImage.src = diceImageArray[6]
-    diceImage.style.top = "50%"
-    diceAnimation.style.animation = "rollDice 500ms"
-    setTimeout(removeAnimation, 500)
-    diceButton.textContent = diceResult(dice)
-    diceButton.style.fontSize = "90px"
+    diceImage.src = diceImageArray[6];
+    diceAnimation.style.animation = "rollDice 500ms";
+    setTimeout(removeAnimation, 500);
+    diceButton.textContent = diceResult(dice);
+    diceButton.style.fontSize = "90px";
   }
 
 }
 
+//Remove animation
 function removeAnimation() {
   diceAnimation.style.animation = "none"
 }
 
+//Generate dice number
 function diceResult(input) {
   return Math.floor(Math.random() * input + 1)
 }
 
 //Score tracker
-
 let score = 20
 const scoreDisplay = document.querySelector("#score-display")
 
@@ -119,7 +133,6 @@ function minusFive() {
 }
 
 //Font size changing buttons
-
 let textAreaSize = document.querySelector("#note")
 
 const fontSmallButton = document.querySelector("#font-btn-s")
